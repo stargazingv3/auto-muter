0da2a56e6168d6f554334e015eef5071f4b5e892
Daniel <danielmohanadhas@gmail.com>
2025-07-28 20:06:27 -0500

    updated ip

--- browser-extension/config.js
+++ browser-extension/config.js
@@ -1,4 +1,4 @@
 // This file contains the configuration for the backend server.
 // Change the host to your server's IP address or domain name when deploying.
-export const BACKEND_HOST = '13.221.120.18';
+export const BACKEND_HOST = '54.242.178.99';
 export const BACKEND_PORT = 8000;
